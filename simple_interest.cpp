3480c44a2729c837f836f681af8a720b537d93cf
NAMBARDAR125 <yasirnambardar125@gmail.com>
2026-02-15 22:36:08 +0530

    CPP program to calculate simple interest and final amount

---
+++ simple_interest.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main(){
+    float p,r,t,x,a;
+    std::cout<<"Enter the principal amount,interest rate and no of years\n";
+    std::cin>>p>>r>>t;
+    x=(p*r*t)/100;
+    a=p+x;
+    std::cout<<x<<" is the simple interest\n";
+    std::cout<<a<<" is the final amount to be paid after"<<t<<" years";
+}
