a2c1e1bc7df41c240fd3360ef31cc2aad336b3d5
NAMBARDAR125 <yasirnambardar125@gmail.com>
2026-03-06 00:05:31 +0530

    Enhanced the readability with cout messages

--- complex_no.cpp
+++ complex_no.cpp
@@ -4,7 +4,6 @@ class complex{
     int r,i,x,y;
     public:
     void getdata(){
-        cout<<"Enter the 1st real and imaginary part of a number"<<endl;
         cin>>r>>i;
     }
     void sum(complex x,complex y){
@@ -17,10 +16,15 @@ class complex{
 };
 int main(){
     complex obj1,obj2,obj3;
+    cout<<"Enter the real and imaginary part of 1st number"<<endl;
     obj1.getdata();
+    cout<<"Enter the real and imaginary part of 2nd number"<<endl;
     obj2.getdata();
+    cout<<"1st complex no is :"<<endl;
     obj1.showdata();
+    cout<<"2nd complex no is :"<<endl;
     obj2.showdata();
+    cout<<"Sum is :"<<endl;
     obj3.sum(obj1,obj2);
     obj3.showdata();
 }
