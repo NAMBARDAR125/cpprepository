a6d49f79ea76db26c6a0882f5986d43d98cde4b7
NAMBARDAR125 <yasirnambardar125@gmail.com>
2026-04-14 17:19:27 +0530

    Added class shape for the program

--- area_function_overloading.cpp
+++ area_function_overloading.cpp
@@ -1,27 +1,31 @@
 #include<iostream>
-#include<math.h>
+#include<cmath>
 using namespace std;
-int pi=3.141593;
-double area(double r){
+double pi=3.141593;
+class Shape{
+public:
+void area(double r){
     cout<<"The area of the circle is : "<<pi*r*r<<endl;
 }
-double area(double l,double b){
-    cout<<"The area of the rectangle is "<<0.5*l*b<<endl;
+void area(double l,double b){
+    cout<<"The area of the rectangle is "<<l*b<<endl;
 }
-double area(double c,double d,double e){
+void area(double c,double d,double e){
     double s=(d+e+c)/2;
     double x=sqrt(s*(s-c)*(s-d)*(s-e));
-    cout<<"The area of the triangle is : "<<x;
+    cout<<"The area of the triangle is : "<<x<<endl;
 }
+};
 int main(){
+    Shape shape;
     double c,d,e,r,l,b;
     cout<<"Enter the radius for area"<<endl;
     cin>>r;
-    area(r);
+    shape.area(r);
     cout<<"Enter the length and breadth for area"<<endl;
     cin>>l>>b;
-    area(l,b);
-     cout<<"Enter the 1st,2nd and 3rd side of the triangle for area"<<endl;
+    shape.area(l,b);
+    cout<<"Enter the 1st,2nd and 3rd side of the triangle for area"<<endl;
     cin>>c>>d>>e;
-    area(c,d,e);
+    shape.area(c,d,e);
 }
